4eb5883096cf32c547ff4900db7b925a5475abba
Alex Fiorenza <alexfiorenza2012@gmail.com>
2022-10-17 16:52:27 -0300

    Added mint nft function

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,17 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: '0.8.17',
+  networks: {
+    mumbai: {
+      url: process.env.TESTNET_RPC,
+      accounts: [process.env.PRIVATE_KEY as string],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.POLYGON_API_KEY,
+  },
 };
 
 export default config;

b3a029302673272b4ee0473ca1a846ad7b8daaf2
Alex Fiorenza <alexfiorenza2012@gmail.com>
2022-11-02 16:49:44 -0300

    Started implementation of buyNftWithDai

--- README.md
+++ README.md
@@ -1,13 +1,57 @@
-# Sample Hardhat Project
+# Collecting Services Contracts
 
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
+Collecting Services are a set of contracts that allows users to buy and mint nfts as dues to be paid
 
-Try running some of the following tasks:
+For more information visit [Collecting Services 📮](https://www.collecting.services/)
+
+### Supported tokens 💰
+
+-   MATIC
+-   USDT
+-   USDC
+-   DAI
+
+## Testing 🧪
+
+All testing is done using [Polygon Mumbai Testnet](https://mumbai.polygonscan.com/)
+
+<!-- ### Last stable testnet contracts for GOERLI ⚙️
+
+-   [SWAPPER 🔄](https://goerli.etherscan.io/address/0x63676f1Aaad4C5a9443Fb3bd1FEF6611F6A5E84E): 0x63676f1Aaad4C5a9443Fb3bd1FEF6611F6A5E84E
+-   [TREASURE 🤑](https://goerli.etherscan.io/address/0x0CDc2A86A61935E4AB920054Fd204a6bB713BDC4): 0x0CDc2A86A61935E4AB920054Fd204a6bB713BDC4 -->
+
+### Contract addresses for Mumbai Testnet ⚙️
+
+| Tokens |                                  Addresses |
+| ------ | -----------------------------------------: |
+| DAI    | 0x001B3B4d0F3714Ca98ba10F6042DaEbF0B1B7b6F |
+| USDT   | 0x2DB274b9E5946855B83e9Eac5aA6Dcf2c68a95F3 |
+| USDC   | 0xe6b8a5CF854791412c1f6EFC7CAf629f5Df1c747 |
+
+### Contract addresses for POLYGON Mainnet 🟣
+
+| Tokens |                                  Addresses |
+| ------ | -----------------------------------------: |
+| DAI    | 0x6B175474E89094C44Da98b954EedeAC495271d0F |
+| USDT   | 0xdAC17F958D2ee523a2206206994597C13D831ec7 |
+| USDC   | 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48 |
+
+### How to use 🤔
+
+1.  Create your own .env file
+2.  Add the specified keys to your .env file
+3.  Run `yarn install` or `npm install`
+4.  Run the following command to compile and deploy the contract:
 
 ```shell
-npx hardhat help
-npx hardhat test
-REPORT_GAS=true npx hardhat test
-npx hardhat node
-npx hardhat run scripts/deploy.ts
+npx hardhat compile
+npm run deploy:goerli
 ```
+
+### Environment variables 📝
+
+| Item            |                           Value |
+| --------------- | ------------------------------: |
+| TESTNET_RPC     |     Add your alchemy RPC server |
+| POLYGON_API_KEY | API Key provided by polygonscan |
+| PRIVATE_KEY     |         Your wallet private key |
--- contracts/CustodialNFT.sol
+++ contracts/CustodialNFT.sol
@@ -4,6 +4,7 @@ pragma experimental ABIEncoderV2;
 import "./Base64.sol";
 import "./jsonParser.sol";
 import "./Strings.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 // NFTExample ={
 //     legalContractBroker:'0xx',
@@ -30,6 +31,13 @@ struct Receipt {
     uint256 tokenId;
     uint256 transactionCompleted;
 }
+struct TokenMetadata {
+    address owner;
+    uint256 amount;
+    uint256 brokerFee;
+    address broker;
+    string token;
+}
 
 contract CustodialNFT {
     NFTReceiptsCS private nftReceiptsCS;
@@ -54,44 +62,40 @@ contract CustodialNFT {
         return myJson;
     }
 
-    function buyNFT(uint256 tokenId) public payable returns (address) {
-        string memory metadata = getNFTMetaData(tokenId);
-        address contractOwner;
-        address broker;
-        uint256 amount;
-        uint256 brokerFee;
-        NFTMetadata[] memory myArray = JsonParser.proccessJSON(
-            metadata,
-            JsonParser.calculateQuantity(8)
+    function buyNFTWithDAI(uint256 tokenId, uint256 amountToPay) public {
+        TokenMetadata memory tokenMetadata = getAsStructToken(tokenId);
+        require(
+            tokenMetadata.amount == amountToPay,
+            "You are not paying the right amount"
         );
-        for (uint256 i = 0; i < myArray.length; i++) {
-            if (Strings.compareStrings(myArray[i].key, "owner")) {
-                contractOwner = Strings.toAddress(myArray[i].value);
-            }
-            if (Strings.compareStrings(myArray[i].key, "amount")) {
-                (amount, ) = Strings.strToUint(myArray[i].value);
-            }
-            if (Strings.compareStrings(myArray[i].key, "brokerFee")) {
-                (brokerFee, ) = Strings.strToUint(myArray[i].value);
-            }
-            if (Strings.compareStrings(myArray[i].key, "broker")) {
-                broker = Strings.toAddress(myArray[i].value);
-            }
-        }
-        require(msg.value == amount, "You must pay the exact amount");
-        if (brokerFee > 0) {
-            uint256 fee = calculateFee(amount, brokerFee);
+        require(
+            Strings.compareStrings(tokenMetadata.token, "DAI"),
+            "You are not paying the right token"
+        );
+    }
+
+    function buyNFT(uint256 tokenId) public payable {
+        TokenMetadata memory tokenMetadata = getAsStructToken(tokenId);
+        require(
+            msg.value == tokenMetadata.amount,
+            "You must pay the exact amount"
+        );
+        if (tokenMetadata.brokerFee > 0) {
+            uint256 fee = calculateFee(
+                tokenMetadata.amount,
+                tokenMetadata.brokerFee
+            );
             require(
                 address(this).balance > fee,
                 "Fee is greater than the amount sent"
             );
-            payable(contractOwner).transfer(amount - fee);
-            payable(broker).transfer(fee);
+            payable(tokenMetadata.owner).transfer(tokenMetadata.amount - fee);
+            payable(tokenMetadata.broker).transfer(fee);
         } else {
-            payable(contractOwner).transfer(amount);
+            payable(tokenMetadata.owner).transfer(tokenMetadata.amount);
         }
         nftReceiptsCS.transferFrom(address(this), msg.sender, tokenId);
-        s_receivedReceipts[contractOwner].push(
+        s_receivedReceipts[tokenMetadata.owner].push(
             Receipt(tokenId, block.timestamp)
         );
         s_paidReceipts[msg.sender].push(Receipt(tokenId, block.timestamp));
@@ -120,5 +124,40 @@ contract CustodialNFT {
     {
         //uint256 private percentage = 100; // 1% in basis points
         return (amount * percentage) / 10000;
+    }
+
+    function getAsStructToken(uint256 tokenId)
+        private
+        view
+        returns (TokenMetadata memory)
+    {
+        string memory metadata = getNFTMetaData(tokenId);
+        address contractOwner;
+        address broker;
+        uint256 amount;
+        uint256 brokerFee;
+        string memory token;
+        NFTMetadata[] memory myArray = JsonParser.proccessJSON(
+            metadata,
+            JsonParser.calculateQuantity(8)
+        );
+        for (uint256 i = 0; i < myArray.length; i++) {
+            if (Strings.compareStrings(myArray[i].key, "owner")) {
+                contractOwner = Strings.toAddress(myArray[i].value);
+            }
+            if (Strings.compareStrings(myArray[i].key, "amount")) {
+                (amount, ) = Strings.strToUint(myArray[i].value);
+            }
+            if (Strings.compareStrings(myArray[i].key, "brokerFee")) {
+                (brokerFee, ) = Strings.strToUint(myArray[i].value);
+            }
+            if (Strings.compareStrings(myArray[i].key, "broker")) {
+                broker = Strings.toAddress(myArray[i].value);
+            }
+            if (Strings.compareStrings(myArray[i].key, "token")) {
+                token = myArray[i].value;
+            }
+        }
+        return TokenMetadata(contractOwner, amount, brokerFee, broker, token);
     }
 }
